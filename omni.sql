0acd689c7698e05ac66a7d34423b304739909302
jtrick <git@jtrick.net>
2013-02-17 17:58:20 -0500

    Modifying syntax for Postgres compatability

--- omni.sql
+++ omni.sql
@@ -1,6 +1,6 @@
--- User Access Tracking for the ACE Omniscience System component.
+-- User Access Tracking for the ACE Omniscience Module.
 -- File : 'omni.sql'
--- Database structure for user tracking system.
+-- Database structure for the Omniscience activity tracking system.
 -- Copyright (c)2012 The New Waters Foundation Inc, all rights reserved.  For license see: http://OpenAce.org/license?id=omni.sql
 
 -- --------------------------------------------------------
@@ -17,18 +17,19 @@
 --
 --	The sites registered to the Omni tracking service.
 
-CREATE TABLE "Site_pri" (
+CREATE TABLE Site_pri (
 	"SiteID" SERIAL PRIMARY KEY,
 	"Name" varchar(50) NOT NULL DEFAULT '',
-	"Description" text NOT NULL,
-	"Domain" text NOT NULL,
+	"Description" text NOT NULL DEFAULT '',
+	"Domain" text NOT NULL DEFAULT '',
 	"Registered" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	"Value" float NOT NULL DEFAULT '0',
-	INDEX ("Domain")
+	"Value" float NOT NULL DEFAULT '0'
 );
 
-INSERT INTO Site_pri (SiteID, Name, Description) VALUES 
-('1', 'Test Site', 'Not a real website, used for testing purposes.');
+CREATE INDEX "Domain_index" ON Site_pri ("SiteID" DESC NULLS LAST);
+
+INSERT INTO Site_pri ("SiteID", "Name", "Description", "Domain") VALUES 
+('1', 'Test Site', 'Not a real website, used for testing purposes.', 'example.com');
 
 -- --------------------------------------------------------
 
@@ -37,9 +38,9 @@ INSERT INTO Site_pri (SiteID, Name, Description) VALUES
 --
 --	All keys currently used in this system, in whatever form they are passed.
 
-CREATE TABLE "Key_pri" (
+CREATE TABLE Key_pri (
 	"PriKeyID" SERIAL PRIMARY KEY,
-	"OldKeyID" integer NOT NULL DEFAULT '0',
+	"OldKeyID" integer NOT NULL DEFAULT '0' REFERENCES Key_pri,
 	"PriKey" varchar(64) NOT NULL DEFAULT '',
 	"OldKey" varchar(64) NOT NULL DEFAULT '',
 	"PubKey" varchar(64) NOT NULL DEFAULT '',
@@ -47,27 +48,28 @@ CREATE TABLE "Key_pri" (
 	"IP" varchar(50) NOT NULL DEFAULT '',
 	"IpID" integer NOT NULL DEFAULT '0',
 	"RemoteIP" varchar(50) NOT NULL DEFAULT '',
-	"ProxyStr" text NOT NULL,
+	"ProxyStr" text NOT NULL DEFAULT '',
 	"SiteID" integer NOT NULL DEFAULT '0',
 	"PageID" integer NOT NULL DEFAULT '0',
-	"Url" text NOT NULL,
-	"BrowserString" text NOT NULL,
+	"Url" text NOT NULL DEFAULT '',
+	"BrowserString" text NOT NULL DEFAULT '',
 	"BrowserID" integer NOT NULL DEFAULT '0',
 	"HitID" integer NOT NULL DEFAULT '0',
 	"ConnectionID" integer NOT NULL DEFAULT '0',
 	"DeviceID" integer NOT NULL DEFAULT '0',
 	"PrintID" integer NOT NULL DEFAULT '0',
 	"UserID" integer NOT NULL DEFAULT '0',
-	"Cookies" text NOT NULL,
+	"Cookies" text NOT NULL DEFAULT '',
 	"CookiesEnabled" varchar(3) NOT NULL DEFAULT '',
 	"CreateTime" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	"Duration" timestamp NOT NULL DEFAULT '00:00:00',
+	"Duration" timestamp NOT NULL DEFAULT 'epoch',
 	"Active" boolean NOT NULL DEFAULT '1',
-	"Value" float NOT NULL DEFAULT '0',
-	INDEX ("PubKey")
+	"Value" float NOT NULL DEFAULT '0'
 );
 
-INSERT INTO Key_pri (PriKeyID, PriKey, OldKeyID) VALUES 
+CREATE INDEX "PubKey-index" ON Key_pri ("PubKey" DESC NULLS LAST);
+
+INSERT INTO Key_pri ("PriKeyID", "PriKey", "OldKeyID") VALUES 
 ('1', 'New_Connection_--_No_Key', '1');
 
 -- --------------------------------------------------------
@@ -77,31 +79,31 @@ INSERT INTO Key_pri (PriKeyID, PriKey, OldKeyID) VALUES
 --
 --	All hits made to sites in this system.
 
-CREATE TABLE "Hit_pri" (
+CREATE TABLE Hit_pri (
 	"HitID" SERIAL PRIMARY KEY,
 	"PriKeyID" integer NOT NULL DEFAULT '0',
 	"SiteID" integer NOT NULL DEFAULT '0',
 	"IpID" integer NOT NULL DEFAULT '0',
 	"IP" varchar(50) NOT NULL DEFAULT '',
-	"Url" text NOT NULL,
+	"Url" text NOT NULL DEFAULT '',
 	"PageID" integer NOT NULL DEFAULT '0',
 	"BrowserID" integer NOT NULL DEFAULT '0',
-	"BrowserString" text NOT NULL,
+	"BrowserString" text NOT NULL DEFAULT '',
 	"DeviceID" integer NOT NULL DEFAULT '0',
-	"DeviceString" text NOT NULL,
+	"DeviceString" text NOT NULL DEFAULT '',
 	"ConnectionID" integer NOT NULL DEFAULT '0',
 	"PrintID" integer NOT NULL DEFAULT '0',
 	"UserID" integer NOT NULL DEFAULT '0',
-	"Sender" text NOT NULL,
-	"Cookies" text NOT NULL,
+	"Sender" text NOT NULL DEFAULT '',
+	"Cookies" text NOT NULL DEFAULT '',
 	"CookiesEnabled" varchar(3) NOT NULL DEFAULT '',
-	"Data" text NOT NULL,
-	"Times" text NOT NULL,
-	"InitLog" text NOT NULL,
-	"PingLog" text NOT NULL,
-	"ClientLog" text NOT NULL,
-	"ClientTimes" text NOT NULL,
-	"ClientWindow" text NOT NULL,
+	"Data" text NOT NULL DEFAULT '',
+	"Times" text NOT NULL DEFAULT '',
+	"InitLog" text NOT NULL DEFAULT '',
+	"PingLog" text NOT NULL DEFAULT '',
+	"ClientLog" text NOT NULL DEFAULT '',
+	"ClientTimes" text NOT NULL DEFAULT '',
+	"ClientWindow" text NOT NULL DEFAULT '',
 	"Value" float NOT NULL DEFAULT '0',
 	"OmniStart" timestamp NOT NULL DEFAULT 0,
 	"TimeStamp" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -114,20 +116,20 @@ CREATE TABLE "Hit_pri" (
 --
 --	All open connections in this system.  Used as the central point of access for variables and screen behavior. Enables multiple simultaneous users or views from same window.
 
-CREATE TABLE "Connection_pri" (
+CREATE TABLE Connection_pri (
 	"ConnectionID" SERIAL PRIMARY KEY,
 	"PrintID" integer NOT NULL DEFAULT '0',
 	"UserID" integer NOT NULL DEFAULT '0',
 	"IpID" integer NOT NULL DEFAULT '0',
 	"SiteID" integer NOT NULL DEFAULT '0',
-	"Sender" text NOT NULL,
+	"Sender" text NOT NULL DEFAULT '',
 	"BrowserID" integer NOT NULL DEFAULT '0',
 	"DeviceID" integer NOT NULL DEFAULT '0',
 	"Active" boolean NOT NULL DEFAULT '0',
 	"Value" float NOT NULL DEFAULT '0',
 	"StartConnectionTime" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	"LastActionTime" timestamp NOT NULL DEFAULT 0,
-	"Duration" timestamp NOT NULL DEFAULT '00:00:00'
+	"LastActionTime" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	"Duration" timestamp NOT NULL DEFAULT 'epoch'
 );
 
 -- INSERT INTO Connection_pri (ConnectionID, UserID, UserIP, BrowserID, Active, Value) VALUES 
@@ -140,7 +142,7 @@ CREATE TABLE "Connection_pri" (
 --
 --	All activity streamed to this system.
 
-CREATE TABLE "Activity_log" (
+CREATE TABLE Activity_log (
 	"LogID" SERIAL PRIMARY KEY,
 	"PriKeyID" integer NOT NULL DEFAULT '0',
 	"HitID" integer NOT NULL DEFAULT '0',
@@ -149,17 +151,17 @@ CREATE TABLE "Activity_log" (
 	"IP" varchar(50) NOT NULL DEFAULT '',
 	"IpID" integer NOT NULL DEFAULT '0',
 	"RemoteIP" varchar(50) NOT NULL DEFAULT '',
-	"FwdIpStr" text NOT NULL,
+	"FwdIpStr" text NOT NULL DEFAULT '',
 	"BrowserID" integer NOT NULL DEFAULT '0',
-	"BrowserString" text NOT NULL,
+	"BrowserString" text NOT NULL DEFAULT '',
 	"DeviceID" integer NOT NULL DEFAULT '0',
-	"DeviceString" text NOT NULL,
+	"DeviceString" text NOT NULL DEFAULT '',
 	"SiteID" integer NOT NULL DEFAULT '0',
 	"PrintID" integer NOT NULL DEFAULT '0',
 	"UserID" integer NOT NULL DEFAULT '0',
-	"Data" text NOT NULL,
-	"Log" text NOT NULL,
-	"Times" text NOT NULL,
+	"Data" text NOT NULL DEFAULT '',
+	"Log" text NOT NULL DEFAULT '',
+	"Times" text NOT NULL DEFAULT '',
 	"Value" float NOT NULL DEFAULT '0',
 	"TimeSent" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -167,22 +169,22 @@ CREATE TABLE "Activity_log" (
 -- --------------------------------------------------------
 
 
---	Event_typ
+--	Pattern_typ
 --
---	The events and actions available to users. 
+--	The patterns that translate into custom events. 
 
-CREATE TABLE "Event_typ" (
-	"EventTyp" SERIAL PRIMARY KEY,
+CREATE TABLE Pattern_typ (
+	"PatternTyp" SERIAL PRIMARY KEY,
 	"Name" varchar(50) NOT NULL DEFAULT '',
-	"Description" text NOT NULL,
+	"Description" text NOT NULL DEFAULT '',
 	"Code" varchar(10) NOT NULL DEFAULT '',
-	"Json" text NOT NULL,
+	"Json" text NOT NULL DEFAULT '',
 	"Value" float NOT NULL DEFAULT '0',
-	UNIQUE (Code)
+	UNIQUE ("Code")
 );
 
-INSERT INTO Event_typ (EventTyp, Name, Description, Code, Value) VALUES 
-('1', 'Hover Wary', 'When a user hovers over an item but does not click on it.', 'hov-wary', '0');
+INSERT INTO Pattern_typ ("PatternTyp", "Name", "Description", "Code", "Value") VALUES 
+('1', 'Hover Wary', 'When a user hovers over an item for a period of time but does not click on it.', 'hov-wary', '0');
 
 -- --------------------------------------------------------
 
@@ -191,9 +193,9 @@ INSERT INTO Event_typ (EventTyp, Name, Description, Code, Value) VALUES
 --
 --	All events and actions triggered by users and registered in this system.
 
-CREATE TABLE "Event_log" (
+CREATE TABLE Event_log (
 	"LogID" SERIAL PRIMARY KEY,
-	"EventTyp" integer NOT NULL DEFAULT '0',
+	"PatternTyp" integer NOT NULL DEFAULT '0' REFERENCES Pattern_typ,
 	"ConnectionID" integer NOT NULL DEFAULT '0',
 	"HitID" integer NOT NULL DEFAULT '0',
 	"PrintID" integer NOT NULL DEFAULT '0',
@@ -209,7 +211,7 @@ CREATE TABLE "Event_log" (
 --
 --	All calls made from sites in this system.
 
-CREATE TABLE "Call_pri" (
+CREATE TABLE Call_pri (
 	"CallID" SERIAL PRIMARY KEY,
 	"SiteID" integer NOT NULL DEFAULT '0',
 	"PriKeyID" integer NOT NULL DEFAULT '0',
@@ -220,10 +222,10 @@ CREATE TABLE "Call_pri" (
 	"HitID" integer NOT NULL DEFAULT '0',
 	"IP" varchar(50) NOT NULL DEFAULT '',
 	"BrowserID" integer NOT NULL DEFAULT '0',
-	"BrowserString" text NOT NULL,
-	"Url" text NOT NULL,
-	"Sender" text NOT NULL,
-	"cmd" text NOT NULL,
+	"BrowserString" text NOT NULL DEFAULT '',
+	"Url" text NOT NULL DEFAULT '',
+	"Sender" text NOT NULL DEFAULT '',
+	"cmd" text NOT NULL DEFAULT '',
 	"Value" float NOT NULL DEFAULT '0'
 );
 
@@ -234,7 +236,7 @@ CREATE TABLE "Call_pri" (
 --
 --	Listing of all IP addresses used to access this system and relevant GeoLocation data for them.
 
-CREATE TABLE "IpData_pri" (
+CREATE TABLE IpData_pri (
 	"IpID" SERIAL PRIMARY KEY,
 	"IP" varchar(50) NOT NULL DEFAULT '',
 	"ServerName" varchar(150) NOT NULL DEFAULT '',
@@ -264,17 +266,17 @@ CREATE TABLE "IpData_pri" (
 --
 --	The various browser headers that have been received to this server.
 
-CREATE TABLE "Browser_pri" (
+CREATE TABLE Browser_pri (
 	"BrowserID" SERIAL PRIMARY KEY,
 	"Name" varchar(50) NOT NULL DEFAULT '',
-	"Description" text NOT NULL,
-	"Header" text NOT NULL,
+	"Description" text NOT NULL DEFAULT '',
+	"Header" text NOT NULL DEFAULT '',
 	"Registered" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"Value" float NOT NULL DEFAULT '0'
 );
 
 INSERT INTO Browser_pri (BrowserID, Name, Description) VALUES 
-('1', 'Null', 'The DEFAULT method of interaction, html interface via any standards-compliant internet browser.');
+('1', 'Generic', 'The DEFAULT generic browser, html interface via any standards-compliant internet browser.');
 
 -- --------------------------------------------------------
 
@@ -283,13 +285,13 @@ INSERT INTO Browser_pri (BrowserID, Name, Description) VALUES
 --
 --	The various device profiles that have been received to this server.
 
-CREATE TABLE "Device_pri" (
+CREATE TABLE Device_pri (
 	"DeviceID" SERIAL PRIMARY KEY,
 	"Name" varchar(50) NOT NULL DEFAULT '',
-	"Description" text NOT NULL,
-	"Screen" text NOT NULL,
-	"Props" text NOT NULL,
-	"Vars" text NOT NULL,
+	"Description" text NOT NULL DEFAULT '',
+	"Screen" text NOT NULL DEFAULT '',
+	"Props" text NOT NULL DEFAULT '',
+	"Vars" text NOT NULL DEFAULT '',
 	"Registered" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"Value" float NOT NULL DEFAULT '0'
 );
@@ -304,12 +306,12 @@ INSERT INTO Device_pri (DeviceID, Name, Description) VALUES
 --
 --	Pages and views that can be accessed through a web site.
 
-CREATE TABLE "Page_pri" (
+CREATE TABLE Page_pri (
 	"NavID" SERIAL PRIMARY KEY,
-	"SiteID" integer NOT NULL DEFAULT '0',
+	"SiteID" integer NOT NULL DEFAULT '0' REFERENCES Site_pri,
 	"Name" varchar(50) NOT NULL DEFAULT '',
-	"Description" text NOT NULL,
-	"Attributes" text NOT NULL,
+	"Description" text NOT NULL DEFAULT '',
+	"Attributes" text NOT NULL DEFAULT '',
 	"Code" varchar(50) NOT NULL DEFAULT '',
 	"Value" float NOT NULL DEFAULT '0',
 	UNIQUE (Code)
@@ -329,11 +331,11 @@ CREATE TABLE "Page_pri" (
 --
 --	All registered users logged by this system.
 
-CREATE TABLE "User_pri" (
+CREATE TABLE User_pri (
 	"UserID" SERIAL PRIMARY KEY,
 	"Name" varchar(50) NOT NULL DEFAULT '',
 	"SiteKey" varchar(50) NOT NULL DEFAULT '',
-	"Description" text NOT NULL,
+	"Description" text NOT NULL DEFAULT '',
 	"Value" float NOT NULL DEFAULT '0',
 	PRIMARY KEY  ("UserID")
 );
@@ -348,13 +350,13 @@ INSERT INTO User_pri (UserID, Name, SiteKey, Description, Value) VALUES
 --
 --	All temporary user profiles accessing this system. Will convert to actual user(s) when associated to account(s)
 
-CREATE TABLE "Print_pri" (
+CREATE TABLE Print_pri (
 	"PrintID" SERIAL PRIMARY KEY,
 	"Name" varchar(50) NOT NULL DEFAULT '',
 	"Cookie" varchar(50) NOT NULL DEFAULT '',
-	"BrowserString" text NOT NULL,
-	"BrowserID" integer NOT NULL DEFAULT '0',
-	"Description" text NOT NULL,
+	"BrowserString" text NOT NULL DEFAULT '',
+	"BrowserID" integer NOT NULL DEFAULT '0' REFERENCES Browser_pri,
+	"Description" text NOT NULL DEFAULT '',
 	"Value" float NOT NULL DEFAULT '0',
 	"FirstAccessIP" varchar(50) NOT NULL DEFAULT '',
 	"FirstAccessTime" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -371,10 +373,10 @@ INSERT INTO Print_pri (PrintID, Name, Cookie, BrowserString, Description) VALUES
 --
 --	Links fingerprint profiles to registered Users.
 
-CREATE TABLE "Print_User_ref" (
+CREATE TABLE Print_User_ref (
 	"RefID" SERIAL PRIMARY KEY,
-	"PrintID" integer NOT NULL DEFAULT '0',
-	"UserID" integer NOT NULL DEFAULT '0',
+	"PrintID" integer NOT NULL DEFAULT '0' REFERENCES Print_pri,
+	"UserID" integer NOT NULL DEFAULT '0' REFERENCES User_pri,
 	"Value" float NOT NULL DEFAULT '0'
 );
 
@@ -388,10 +390,10 @@ INSERT INTO Print_User_ref (RefID, PrintID, UserID, Value) VALUES
 --
 --	Links fingerprint profiles to registered IP addresses.
 
-CREATE TABLE "Print_IP_ref" (
+CREATE TABLE Print_IP_ref (
 	"RefID" SERIAL PRIMARY KEY,
-	"PrintID" integer NOT NULL DEFAULT '0',
-	"IpID" integer NOT NULL DEFAULT '0',
+	"PrintID" integer NOT NULL DEFAULT '0' REFERENCES Print_pri,
+	"IpID" integer NOT NULL DEFAULT '0' REFERENCES IpData_pri,
 	"Value" float NOT NULL DEFAULT '0'
 );
 
@@ -405,7 +407,7 @@ INSERT INTO Print_IP_ref (RefID, PrintID, IpID, Value) VALUES
 --
 --	Links Users to their system preferences
 
-CREATE TABLE "User_prefs_pri" (
+CREATE TABLE User_prefs_pri (
 	"UserPrefsID" SERIAL PRIMARY KEY,
 	"UserID" integer NOT NULL DEFAULT '0',
 	"TransitionSpeed" float NOT NULL DEFAULT '.25',
@@ -423,7 +425,7 @@ INSERT INTO User_prefs_pri (UserPrefsID, UserID, TransitionSpeed, Cookies, Value
 --
 --	All session visits made by users.
 
-CREATE TABLE "User_Visit_log" (
+CREATE TABLE User_Visit_log (
 	"LogID" SERIAL PRIMARY KEY,
 	"PrintID" integer NOT NULL DEFAULT '0',
 	"UserIP" varchar(50) NOT NULL DEFAULT '',
@@ -445,13 +447,13 @@ INSERT INTO User_Visit_log (LogID, PrintID, UserIP, ConnectionID, UserID, LoginT
 --
 --	Logging navigation data for each step taken within the system.
 
-CREATE TABLE "User_Nav_log" (
+CREATE TABLE User_Nav_log (
 	"NavLogID" SERIAL PRIMARY KEY,
 	"ConnectionID" integer NOT NULL DEFAULT '0',
 	"PrintID" integer NOT NULL DEFAULT '0',
 	"UserID" integer NOT NULL DEFAULT '0',
 	"SiteID" integer NOT NULL DEFAULT '0',
-	"url" text NOT NULL,
+	"url" text NOT NULL DEFAULT '',
 	"NavID" integer NOT NULL DEFAULT '0',
 	"Time" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"Value" float NOT NULL DEFAULT '0'
@@ -464,10 +466,10 @@ CREATE TABLE "User_Nav_log" (
 --
 --	Various items that can be flagged to abstract complex behavior into simple markers. 
 
-CREATE TABLE "Flag_typ" (
+CREATE TABLE Flag_typ (
 	"FlagTyp" SERIAL PRIMARY KEY,
 	"Name" varchar(50) NOT NULL DEFAULT '',
-	"Description" text NOT NULL,
+	"Description" text NOT NULL DEFAULT '',
 	"Code" varchar(10) NOT NULL DEFAULT '',
 	"Value" float NOT NULL DEFAULT '0',
 	UNIQUE (Code)
@@ -483,12 +485,12 @@ INSERT INTO Flag_typ (FlagTyp, Name, Description, Code, Value) VALUES
 --
 --	A block of time, and associated items.
 
-CREATE TABLE "TimeBlock_pri" (
+CREATE TABLE TimeBlock_pri (
 	"TimeBlockID" SERIAL PRIMARY KEY,
 	"PrintID" integer NOT NULL DEFAULT '0',
 	"Name" varchar(50) NOT NULL DEFAULT '',
-	"Description" text NOT NULL,
-	"Note" text NOT NULL,
+	"Description" text NOT NULL DEFAULT '',
+	"Note" text NOT NULL DEFAULT '',
 	"Date" date NOT NULL DEFAULT '0000-00-00',
 	"StartTime" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"EndTime" timestamp NOT NULL DEFAULT 0,
