9bb52a1597e03a7ce073be9d6a010d160797dcde
jtrick <git@jtrick.net>
2013-03-08 20:56:11 -0500

    Added basic admin file structure.

---
+++ admin.js
@@ -0,0 +1,8 @@
+// omni/admin.js  Copyright (c)2013 Codapt LLC, all rights reserved.  For license see: http://OpenAce.org/license?id=omni/admin.js
+
+
+$(document).ready(function omniAdminReady() {
+	
+	
+	
+}(jQuery)); //omniAdminReady()
