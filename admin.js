1ff92c81c128dbc6e2e6d7d0f79d97176fc51d01
Calvin <calpoog@gmail.com>
2013-03-10 22:20:30 -0400

    Created a static web page mockup of Ian's image. Should give a good starting point for other pages as well as templates for widgets such as the cards.

--- admin.js
+++ admin.js
@@ -1,8 +1,8 @@
 // omni/admin.js  Copyright (c)2013 Codapt LLC, all rights reserved.  For license see: http://OpenAce.org/license?id=omni/admin.js
 
-
 $(document).ready(function omniAdminReady() {
-	
-	
+
+    $( "#panels-sect" ).sortable({handle: ".title", opacity: 0.5, cursor: "move"});
+    $( ".card .title" ).disableSelection();
 	
 }(jQuery)); //omniAdminReady()
