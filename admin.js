8be93b6bcc15c8f1473091089d4cbe5079df1efc
Calvin <calpoog@gmail.com>
2013-03-30 12:51:59 -0400

    Added dropdown selection functionality - unfinished

--- admin.js
+++ admin.js
@@ -1,79 +1,127 @@
 // omni/admin.js  Copyright (c)2013 Codapt LLC, all rights reserved.  For license see: http://OpenAce.org/license?id=omni/admin.js
 
-var watch = function(obj, attr, name, callback) {
-    var watchedValue = obj[attr];
-    // add a list of watchers to the object if not present
-    if (!obj.hasOwnProperty("_watchers")) {
-        obj._watchers = {};
-    }
-    // add a watcher to the list for the specific attribute if not present
-    if (!obj._watchers.hasOwnProperty(attr)) {
-        obj._watchers[attr] = {};
-    }
-    // add a watcher with a specific name to this attribute
-    if (!obj._watchers[attr].hasOwnProperty(name)) {
-        obj._watchers[attr][name] = callback;
+$(document).ready(function omniAdminReady() {
+
+    // Adds a watcher to obj on attr with watcher name which executes callback when the attr is set
+    var watch = function(obj, attr, name, callback) {
+        var watchedValue = obj[attr];
+        // add a list of watchers to the object if not present
+        if (!obj.hasOwnProperty("_watchers")) {
+            obj._watchers = {};
+        }
+        // add a watcher to the list for the specific attribute if not present
+        if (!obj._watchers.hasOwnProperty(attr)) {
+            obj._watchers[attr] = {};
+        }
+        // add a watcher with a specific name to this attribute
+        if (!obj._watchers[attr].hasOwnProperty(name)) {
+            obj._watchers[attr][name] = callback;
+        }
+
+        Object.defineProperty(obj, attr, {
+            set: function(value) {
+                watchedValue = value;
+                for (var watcher in this._watchers[attr]) {
+                    this._watchers[attr][watcher].apply(this, [value]);
+                }
+            },
+            get: function() {
+                return watchedValue;
+            }
+        });
+
+        // do an initial call to the watcher for the first value
+        obj[attr] = watchedValue;
     }
 
-    Object.defineProperty(obj, attr, {
-        set: function(value) {
-            watchedValue = value;
-            for (watcher in this._watchers[attr]) {
-              this._watchers[attr][watcher].apply(this, [value]);
+    // Removes the watcher on obj's attr with the specified name
+    var unwatch = function(obj, attr, name) {
+        if (obj.hasOwnProperty("_watchers") && obj._watchers.hasOwnProperty(attr) &&
+            obj._watchers[attr].hasOwnProperty(name)) {
+            delete  obj._watchers[attr][name];
+        }
+    }
+
+    $.widget("ui.dropdownSelect", {
+        _create: function() {
+            var dropdown = $('<ul class="select-dropdown"></ul>'),
+                option,
+                i,
+                domeElem = this.element,
+                selections = this.options.selections;
+
+            for (i = 0; i < selections.length; i++) {
+                option = $('<li></li>');
+                option.text(selections[i].text);
+                option.click(selections[i].click);
+                option.appendTo(dropdown);
             }
-        },
-        get: function() {
-            return watchedValue;
+
+            var pos = domeElem.position(),
+                height = domeElem.outerHeight();
+
+            dropdown.css({
+                position: "absolute",
+                top: height + "px",
+                left: 0
+            }).hide();
+
+            dropdown.appendTo(this.element);
+
+            domeElem.css({position: "relative"}).click(function() {
+                 $(this).find('.select-dropdown').show();
+            });
         }
     });
 
-    // do an initial call to the watcher for the first value
-    obj[attr] = watchedValue;
-}
+    // Pattern Card widget definition
+    $.widget("ui.patternCard", {
+        options: {},
+        setStatistic: function(stat) {
+            this.element.find('.body .large').text(stat);
+        },
+        _create: function() {
+            var options = this.options,
+                domElem = this.element,
+                cardTemp = _.template('' +
+                    '<div class="title">' +
+                        '<div class="{{icon}}"></div>' +
+                        '<div>{{title}}</div>' +
+                    '</div>' +
+                    '<div class="body">' +
+                        '<div class="timeframe">In the <span class="underline">{{timeframe}}</span>:</div>' +
+                        '<div class="large">{{statistic}}</div>' +
+                        '<div class="tag">Tag: {{tag}}</div>' +
+                    '</div>'
+                );
 
-var unwatch = function(obj, attr, name) {
-    if (obj.hasOwnProperty("_watchers") && obj._watchers.hasOwnProperty(attr) &&
-        obj._watchers[attr].hasOwnProperty(name)) {
-        delete  obj._watchers[attr][name];
-    }
-}
+            domElem.addClass("card").html(cardTemp(options));
 
-$.widget("ui.patternCard", {
-    options: {},
-    setStatistic: function(stat) {
-        this.element.find('.body .large').text(stat);
-    },
-    _create: function() {
-        var options = this.options,
-            domElem = this.element,
-            cardTemp = _.template(''
-                + '<div class="title">'
-                    + '<div class="{{icon}}"></div>'
-                    + '<div>{{title}}</div>'
-                + ' </div>'
-                + '<div class="body">'
-                    + '<div class="timeframe">In the <span class="underline">{{timeframe}}</span>:</div>'
-                    + '<div class="large">{{statistic}}</div>'
-                    + '<div class="tag">Tag: {{tag}}</div>'
-                    + '<div class="card-btns">'
-                        + '<div class="graphs-card-btn"></div>'
-                        + '<div class="duplicate-card-btn"></div>'
-                        + '<div class="share-card-btn"></div>'
-                        + '<div class="delete-card-btn"></div>'
-                    + '</div>'
-                + '</div>'
-        );
+            domElem.children(".title").disableSelection();
 
-        domElem.addClass("card").html(cardTemp(options));
+            // add dropdown to timeframe
+            domElem.find('.timeframe .underline').dropdownSelect({
+                selections: [
+                    {
+                        text: "Last hour",
+                        click: function() {alert("hey");}
+                    },
+                    {
+                        text: "Last 12 hours",
+                        click: function() {alert("hey2");}
+                    },
+                    {
+                        text: "Last 24 hours",
+                        click: function() {alert("hey3");}
+                    }
+                ]
+            });
 
-        domElem.children(".title").disableSelection();
-        if (options.hasOwnProperty("titleColor")) {
-            domElem.children(".title").css("backgroundColor", options.titleColor);
+            if (options.hasOwnProperty("titleColor")) {
+                domElem.children(".title").css("backgroundColor", options.titleColor);
+            }
         }
-    }
-});
-
-$(document).ready(function omniAdminReady() {
+    });
 
     $("#card1").patternCard({
         title: "Time Spent On Homepage Prior ...",
