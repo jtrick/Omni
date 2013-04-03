3c98a0bdb33140b923df9a95ee212951d10699c0
Calvin <calpoog@gmail.com>
2013-04-03 18:40:26 -0400

    Implemented separate card widget to not reuse code. patternCard widget creates a card widget and then fills it with pattern specific content. Now we can create all kinds of cards with different bodies without reusing code. Also added selection dropdowns and custom tooltips. Updated the patterns page to reflect Ian's most recent changes.

--- admin.js
+++ admin.js
@@ -32,7 +32,7 @@ $(document).ready(function omniAdminReady() {
 
         // do an initial call to the watcher for the first value
         obj[attr] = watchedValue;
-    }
+    };
 
     // Removes the watcher on obj's attr with the specified name
     var unwatch = function(obj, attr, name) {
@@ -40,150 +40,353 @@ $(document).ready(function omniAdminReady() {
             obj._watchers[attr].hasOwnProperty(name)) {
             delete  obj._watchers[attr][name];
         }
-    }
+    };
 
+    // Show any kind of popup, closes all others and stops even from bubbling to html which closes open popups
+    var showPopup = function(e, id) {
+        $('.popup').hide();
+        $(id).show();
+        e.stopPropagation();
+    };
+    var closePopups = function() {
+        $('.popup').hide();
+    };
+
+    // generates a new id for dynamic page elements
+    var nextId = (function() {
+        // static private variable
+        var staticId = 0,
+            getNext = function() {
+                return staticId++;
+            }
+
+        return getNext;
+    })();
+
+    // Selectable drop down widgets definition
     $.widget("ui.dropdownSelect", {
         _create: function() {
-            var dropdown = $('<ul class="select-dropdown"></ul>'),
+            var id = nextId(),
+                theme = this.options.theme || "light",
+                dropdown = $('<ul class="select-dropdown ' + theme + ' popup" id="select-dropdown' + id + '"></ul>'),
                 option,
                 i,
-                domeElem = this.element,
-                selections = this.options.selections;
+                domElem = this.element,
+                selections = this.options.selections,
+                height = domElem.outerHeight();
+
+            var performClick = function(callback, element) {
+                return function(e) {
+                    callback.apply(element);
+                    closePopups();
+                    e.stopPropagation();
+                };
+            }
 
             for (i = 0; i < selections.length; i++) {
                 option = $('<li></li>');
                 option.text(selections[i].text);
-                option.click(selections[i].click);
+                option.click(performClick(selections[i].click, option));
                 option.appendTo(dropdown);
+                if (i == 0) {
+                    option.append('<div class="arrow"></div>');
+                }
             }
 
-            var pos = domeElem.position(),
-                height = domeElem.outerHeight();
+            domElem.css({position: "relative"}).append(dropdown);
 
             dropdown.css({
-                position: "absolute",
-                top: height + "px",
-                left: 0
+                top: (height + 15) + "px",
+                left: 0,
+                width: this.options.width + "px"
             }).hide();
 
-            dropdown.appendTo(this.element);
-
-            domeElem.css({position: "relative"}).click(function() {
-                 $(this).find('.select-dropdown').show();
+            domElem.addClass("pointer").click(function(e) {
+                showPopup(e, '#select-dropdown' + id);
             });
         }
     });
 
-    // Pattern Card widget definition
+    // custom tooltip for simplicity and to solve jumping around issue
+    $.widget("ui.omniTooltip", {
+        _create: function() {
+            var domElem = this.element,
+                id = nextId(),
+                timeOut = null,
+                timeRunning = false,
+                options = this.options,
+                disabled = false,
+                tt = $('' +
+                '<div class="tooltip" id="ui-tooltip-' + id + '">' +
+                    '<div class="tooltip-content">' +
+                        domElem.data("title") +
+                        '<div class="arrow"></div>' +
+                    '</div>' +
+                '<div>'
+                ).css({width: this.options.width + "px"}).hide();
+
+            // tooltip public functions
+            this.toggle = function() {
+                disabled = !disabled;
+                if (disabled) {
+                    hideTooltip();
+                }
+            };
+
+            var adjustPos = function() {
+                var height = domElem.outerHeight(),
+                    width = domElem.outerWidth(),
+                    pos = domElem.offset(),
+                    left = 0,
+                    top = 0;
+
+                // Position the arrow according to the option
+                if (options.hasOwnProperty("arrowPos")) {
+                    switch (options.arrowPos) {
+                        case "bottom-left":
+                            left -= 33;
+                            break;
+                        case "bottom-center":
+                            left = left - 33 + width / 2;
+                            break;
+                        // default left of box to left of parent
+                        // can add other positions (above/below, right, etc.) if we need to
+                    }
+                }
+                tt.css({
+                    top: (pos.top + height + top + 15) + "px",
+                    left: (pos.left + left) + "px"
+                });
+            };
+
+            var showTooltip = function() {
+                if (!disabled) {
+                    adjustPos();
+                    tt.appendTo('body').fadeIn();
+                }
+            };
+
+            var hideTooltip = function() {
+                $('#ui-tooltip-' + id).fadeOut(function() {
+                    $(this).remove();
+                });
+                if (timeRunning) {
+                    clearTimeout(timeOut);
+                }
+                timeRunning = false;
+            }
+
+            // Display the tooltip after a delay, cancel it if exited before it displays
+            domElem.hover(function() {
+                timeRunning = true;
+                timeOut = setTimeout(showTooltip, 1000);
+            }, hideTooltip);
+        }
+    });
+
+    // Pattern card widget definition
     $.widget("ui.patternCard", {
-        options: {},
-        setStatistic: function(stat) {
-            this.element.find('.body .large').text(stat);
-        },
         _create: function() {
             var options = this.options,
-                domElem = this.element,
-                cardTemp = _.template('' +
-                    '<div class="title">' +
-                        '<div class="{{icon}}"></div>' +
-                        '<div>{{title}}</div>' +
-                    '</div>' +
-                    '<div class="body">' +
-                        '<div class="timeframe">In the <span class="underline">{{timeframe}}</span>:</div>' +
-                        '<div class="large">{{statistic}}</div>' +
-                        '<div class="tag">Tag: {{tag}}</div>' +
-                    '</div>'
-                );
+                domElem = this.element;
 
-            domElem.addClass("card").html(cardTemp(options));
+            // create the card base
+            domElem.card(options);
 
-            domElem.children(".title").disableSelection();
+            // pattern card specific body content
+            var patternTemp = _.template('' +
+                '<div class="content">' +
+                    '<div class="timeframe">In the <span class="underline">{{timeframe}}</span>:</div>' +
+                    '<div class="large">{{statistic}}</div>' +
+                '</div>' +
+                '<div class="tag">{{tag}}</div>'
+            );
+
+            // set the statistic for this pattern
+            this.setStatistic = function(stat) {
+                this.element.find('.body .large').text(stat);
+            };
+
+            // set the card body content
+            domElem.card("setContent", patternTemp(options));
 
             // add dropdown to timeframe
             domElem.find('.timeframe .underline').dropdownSelect({
                 selections: [
                     {
                         text: "Last hour",
-                        click: function() {alert("hey");}
+                        click: function() {alert($(this).text());}
                     },
                     {
                         text: "Last 12 hours",
-                        click: function() {alert("hey2");}
+                        click: function() {alert($(this).text());}
                     },
                     {
                         text: "Last 24 hours",
-                        click: function() {alert("hey3");}
+                        click: function() {alert($(this).text());}
                     }
-                ]
+                ],
+                width: 150
             });
-
-            if (options.hasOwnProperty("titleColor")) {
-                domElem.children(".title").css("backgroundColor", options.titleColor);
-            }
         }
     });
 
-    $("#card1").patternCard({
+    // Base card widget definition
+    var cardWidget = (function() {
+        // patternCard static vars
+        var curSelection = [];
+
+        // patternCard static methods
+        this.getCurSelection = function() {
+
+        };
+
+        $.widget("ui.card", {
+            _create: function() {
+                // patternCard private vars
+                var selected = false,
+                    options = this.options,
+                    domElem = this.element,
+                    id = nextId();
+
+                // patternCard public methods
+                this.toggleSelect = function() {
+                    selected = !selected;
+                    if (!selected) {
+                        domElem.find('.check').remove();
+                    } else {
+                        $('<div class="check"></div>').appendTo(domElem);
+                    }
+                };
+
+                this.toggleTooltips = function() {
+                    domElem.find('.tooltip').omniTooltip("toggle");
+                };
+
+                this.setContent = function(content) {
+                    domElem.find('.body').html(content);
+                };
+
+                // patternCard private methods
+                var initialize = function() {
+                    var cardTemp = _.template('' +
+                            '<div class="title" data-title="Something">' +
+                                '<div class="{{icon}}"></div>' +
+                                '<div>{{title}}</div>' +
+                            '</div>' +
+                            '<div class="body">' +
+                            '</div>'
+                        );
+
+                    if (!options.hasOwnProperty("icon")) {
+                        options.icon = "";
+                    }
+
+                    // transform the domElem into a card
+                    domElem.addClass("card").html(cardTemp(options)).attr("id", "card" + id);
+
+                    // disable selection of the title and add tooltip for description
+                    domElem.children(".title").disableSelection().omniTooltip();
+
+                    // figure out the best place to click a card to select/deselect
+
+                    // if the title color attribute is set in the options, change it from the default
+                    if (options.hasOwnProperty("titleColor")) {
+                        domElem.children(".title").css("backgroundColor", options.titleColor);
+                    }
+                };
+
+                initialize.apply(this);
+            }
+        });
+    })();
+
+    var buildCards = function(data) {
+        var i, card;
+        for (i = 0; i < data.length; i++) {
+            card = $('<div></div>');
+            $('#panels-sect').append(card);
+            card.patternCard(data[i]);
+        }
+    }
+
+    // Close all popups if a click bubbles up to html
+    $('html').click(closePopups);
+
+    // Add tooltips to card action buttons
+    $('.card-btns div').omniTooltip({arrowPos: "bottom-center"});
+
+    // Add dropdown to the sort button
+    $('#sort-btn').dropdownSelect({
+        selections: [
+            {
+                text: "Icon (Category)",
+                click: function() {alert($(this).text());}
+            },
+            {
+                text: "Importance (Color)",
+                click: function() {alert($(this).text());}
+            },
+            {
+                text: "Custom 1",
+                click: function() {alert($(this).text());}
+            },
+            {
+                text: "Custom 2",
+                click: function() {alert($(this).text());}
+            }
+        ],
+        width: 200,
+        theme: "dark"
+    });
+
+    /*  Sample data structure coming from server when the page is first loaded to build the
+        initial cards */
+    var cards = [{
         title: "Time Spent On Homepage Prior ...",
         timeframe: "last hour",
         statistic: "11.78s",
         tag: "Display this data daily",
-        icon: "title-icon",
         titleColor: "#e75555"
-    });
-    $("#card2").patternCard({
+    },
+    {
         title: "Number Of New Logins Created",
         timeframe: "last 6 hours",
         statistic: "9",
         tag: "Display this data every Monday",
         icon: "title-icon-2"
-    });
-    $("#card3").patternCard({
+    },
+    {
         title: "Number Of Unique Guest IDs",
         timeframe: "last 6 hours",
         statistic: "22",
         tag: "Display this data every Monday",
         icon: "title-icon-2"
-    });
-    $("#card4").patternCard({
+    },
+    {
         title: "Ratio New Logins Created/Unique ...",
         timeframe: "last 6 hours",
         statistic: "0.409",
         tag: "Display this data every Monday",
         icon: "title-icon-2"
+    }];
+
+    // Build cards with the fake data
+    buildCards(cards);
+
+    // Make cards sortable
+    $("#panels-sect").sortable({
+        handle: ".title",
+        opacity: 0.5,
+        cursor: "move",
+        start: function(event, ui) {
+            closePopups();
+            ui.item.patternCard('toggleTooltips');
+        },
+        stop: function(event, ui) {
+            // make sure the z-index isn't set to 0 after dragging
+            ui.item.css({"z-index": 999});
+        }
     });
-
-    $("#panels-sect").sortable({handle: ".title", opacity: 0.5, cursor: "move"});
-
-    // testing some watch/unwatch stuff
-    var b = {
-            x: 0,
-            y: 10,
-            newLogins: 6
-        };
-
-    watch(b, "x", "xWatcher", function(value) {
-        $('#main-top-bar').text(value);
-    });
-    watch(b, "y", "yWatcher", function(value) {
-        $('#main-top-bar').text($('#main-top-bar').text() + " " + value);
-    });
-    watch(b, "x", "xWatcher2", function(value) {
-        $('#main-top-bar').text($('#main-top-bar').text() + " " + value);
-    });
-    unwatch(b, "x", "xWatcher2");
-
-    b.x = 20;
-    b.y = "Calvin";
-
-    // actually do something that this might be used for
-    watch(b, "newLogins", "pullNewLogins", function(value) {
-        $('#card2').patternCard("setStatistic", value);
-    });
-
-    // make a timer to update it
-    setInterval(function() {
-        b.newLogins++;
-    }, 2000);
 	
 }(jQuery)); //omniAdminReady()
